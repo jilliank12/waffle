4ffecd6c6a561446b766f44da6475aefebf80afb
JillianKim <jinkim1989@gmail.com>
2015-11-04 03:27:57 +0900

    doing more trees  Changes to be committed: 	modified:   .idea/workspace.xml 	modified:   src/DSinsight/chapter10/BubbleSort.java 	new file:   src/DSinsight/chapter6/AVLTreeNode.java
     Changes not staged for commit:
    	deleted:    out/production/InterviewPrep/FloodFill.class
    deleted:    out/production/InterviewPrep/GroupSum.class
    	deleted:    out/production/InterviewPrep/Pair.class
    	deleted:    out/production/InterviewPrep/floodfillmap.txt

--- src/DSinsight/chapter10/BubbleSort.java
+++ src/DSinsight/chapter10/BubbleSort.java
@@ -4,4 +4,23 @@ package DSinsight.chapter10;
  * Created by Jilliankim on 11/3/15.
  */
 public class BubbleSort {
+
+    void bubbleSort(int a[], int n) {
+        for (int pass = n - 1; pass >= 0; pass--) {
+            for (int i = 0; i < pass -1; i++) {
+                if(a[i] > a[i+1]) {
+                    int temp = a[i];
+                    a[i] = a[i+1];
+                    a[i+1] = temp;
+                }
+            }
+        }
+    }
+
+    void bubbleSortImproved(int a[], int n) {
+        int swapped = 1;
+//        for (int pass = n -1; pass >= 0 && swapped; pass--) {
+//
+//        }
+    }
 }
